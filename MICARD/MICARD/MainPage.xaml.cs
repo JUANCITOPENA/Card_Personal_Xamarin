f6f34ddb1529c80dd394493825a4d1837fd46f01
JUANCITOPENA <JUANCITO.PENA.V@HOTMAIL.COM>
2021-02-26 11:02:56 -0400

    Mi Primer Commit del Proyecto V1.0

---
+++ MICARD/MICARD/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace MICARD
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnYoutube_Clicked(object sender, EventArgs e)
+        {
+            Browser.OpenAsync("https://www.youtube.com/channel/UCSob-3E5z4IHtMF5B4bN-FA?view_as=subscriber", BrowserLaunchMode.SystemPreferred);
+        }
+
+        private void btnVerlinkedin_Clicked(object sender, EventArgs e)
+        {
+            Browser.OpenAsync("https://www.linkedin.com/in/juancitope%C3%B1a/", BrowserLaunchMode.SystemPreferred);
+        }
+
+        private void btnVerTwitter_Clicked(object sender, EventArgs e)
+        {
+            Browser.OpenAsync("https://twitter.com/JuancitoPenaV", BrowserLaunchMode.SystemPreferred);
+        }
+
+        private void btnVerInstagrams_Clicked(object sender, EventArgs e)
+        {
+            Browser.OpenAsync("https://www.instagram.com/juancito.pena.v/", BrowserLaunchMode.SystemPreferred);
+        }
+
+        private void btnVerFacebook_Clicked(object sender, EventArgs e)
+        {
+            Browser.OpenAsync("https://www.facebook.com/juancito.p.v/", BrowserLaunchMode.SystemPreferred);
+        }
+    }
+}
